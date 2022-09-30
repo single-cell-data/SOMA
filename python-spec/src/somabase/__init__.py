3fa8d2fc20110a84739eb9d57d0f6564b23781aa
Paul Fisher <paul.fisher@tiledb.com>
2022-09-30 17:55:36 -0400

    Finalize some infrastructure stuff for the specification.
    - Adds more information to the `pyproject.toml`.
    - Adds a README.
    - formats everything with `black` and `isort`.

--- python-spec/src/somabase/__init__.py
+++ python-spec/src/somabase/__init__.py
@@ -1,1 +1,5 @@
-"""Module base for the Python reference specification of SOMA."""
+"""Module base for the Python reference specification of SOMA.
+
+Types will be defined in their own modules and then imported here for a single
+unified namespace.
+"""
