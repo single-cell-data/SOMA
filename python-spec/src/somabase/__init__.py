e37e6f03dab922cc0a1659b128f79f51b6d19ca3
paul fisher <paul.fisher@tiledb.com>
2022-10-05 12:28:34 -0400

    Add concrete classes and enums for options. (#39)
    Adds the "common interfaces" `IOfN` and `BatchSize`.
    These classes are the exact values that implementations will use,
    i.e. an implementation of the SOMA API will not implement its own
    `IOfN` or any of these other “option” types.
    
    These types are implemented using the `attrs` package, essentially
    an enhanced version of the built-in `dataclasses` package. It is
    very lightweight and already extensively used within the Python
    ecosystem so it will not add much (if anything) to the size of a typical
    installed environment.

--- python-spec/src/somabase/__init__.py
+++ python-spec/src/somabase/__init__.py
@@ -3,3 +3,21 @@
 Types will be defined in their own modules and then imported here for a single
 unified namespace.
 """
+
+from somabase import base
+from somabase import options
+
+SOMAObject = base.SOMAObject
+Collection = base.Collection
+
+IOfN = options.IOfN
+BatchSize = options.BatchSize
+BatchFormat = options.BatchFormat
+
+__all__ = (
+    "SOMAObject",
+    "Collection",
+
+    "IOfN",
+    "BatchSize",
+)
---
+++ python-spec/src/somabase/options.py
@@ -0,0 +1,73 @@
+"""Enums and other types used as options across methods of many types.
+
+These types are *concrete* and should be used as-is as inputs to the various
+SOMA types that require them, not reimplemented by the implementing package.
+"""
+
+from typing import Optional
+
+import attrs
+
+
+@attrs.define(frozen=True)
+class IOfN:
+    """Specifies that a read should return partition ``i`` out of ``n`` total.
+
+    For a read operation that returns ``n`` partitions, the read operation will
+    return the ``i``th partition (zero-indexed) out of ``n`` partitions of
+    approximately equal size.
+    """
+    __slots__ = ()
+
+    i: int = attrs.field()
+    """Which partition to return (zero-indexed)."""
+    n: int = attrs.field()
+    """How many partitions there will be."""
+
+    @i.validator
+    def _validate(self, _, __):
+        del _, __  # Unused.
+        if not 0 <= self.i < self.n:
+            raise ValueError(
+                f"Partition index {self.i} must be in the range [0, {self.n})"
+            )
+
+
+@attrs.define(frozen=True)
+class BatchSize:
+    """Specifies the size of a batch that should be returned from reads.
+
+    Read operations on foundational types return an iterator over "batches" of
+    data, enabling processing of larger-than-core datasets. This class allows
+    you to control what the size of those batches is.
+
+    If none of these options are set, a "reasonable" batch size is determined
+    automatically.
+
+    For example::
+
+        BatchSize(count=100)
+        # Will return batches of 100 elements.
+
+        BatchSize(bytes=1024 ** 2)
+        # Will return batches of up to 1 MB.
+
+        BatchSize()
+        # Will return automatically-sized batches.
+    """
+    __slots__ = ()
+
+    count: Optional[int] = attrs.field(default=None)
+    """``arrow.Table``s with this number of rows will be returned."""
+    bytes: Optional[int] = attrs.field(default=None)
+    """Data of up to this size in bytes will be returned."""
+
+    @count.validator
+    @bytes.validator
+    def _validate(self, attr: attrs.Attribute, value):
+        if not value:
+            return  # None (or 0, which we treat equivalently) is always valid.
+        if value < 0:
+            raise ValueError(f"If set, '{attr.name}' must be positive")
+        if self.count and self.bytes:
+            raise ValueError("Either 'count' or 'bytes' may be set, not both")
