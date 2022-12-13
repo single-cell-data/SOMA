c0febfd7721ad6f0b8f893af144e00f4f8740d19
Paul Fisher <paul.fisher@tiledb.com>
2022-12-13 10:59:47 -0500

    Add remaining fields to base SOMA object.
    - Adds `metadata`, `context`, and `soma_type` methods to `SOMAObject`.
    - Formats and adds `typing_extensions` dependency.

--- python-spec/src/somabase/base.py
+++ python-spec/src/somabase/base.py
@@ -1,15 +1,50 @@
-"""The most fundamental types used by the SOMA project."""
+"""Definitions of the most fundamental types used by the SOMA project.
 
-from typing import MutableMapping, TypeVar
+SOMA users should ordinarily not need to import this module directly; relevant
+members will be exported to the ``somabase`` namespace.
+"""
+
+import abc
+from typing import Any, MutableMapping, TypeVar
+
+from typing_extensions import Literal, LiteralString
 
 
-class SOMAObject:
+class SOMAObject(metaclass=abc.ABCMeta):
     """A sentinel interface indicating that this type is a SOMA object."""
 
     __slots__ = ()
 
-    # TODO: Consider if there are any more fundamental behaviors that should be
-    # pushed down to the SOMAObject level.
+    @property
+    def context(self) -> Any:
+        """A value storing implementation-specific configuration information.
+
+        This contains long-lived (i.e., not call-specific) information that is
+        used by the SOMA implementation to access storage. This may include
+        things like credentials, endpoint locations, or database connections.
+
+        End users should treat this as an opaque value. While it may be passed
+        from an existing SOMA object to be used in the creation of a new SOMA
+        object, it should not be inspected.
+        """
+        return None
+
+    @property
+    @abc.abstractmethod
+    def metadata(self) -> MutableMapping[str, Any]:
+        """The metadata of this SOMA object.
+
+        The returned value directly references the stored metadata; reads from
+        and writes to it (provided the object is opened) are reflected in
+        storage.
+        """
+        raise NotImplementedError()
+
+    @property
+    @abc.abstractmethod
+    def soma_type(self) -> LiteralString:
+        """A string describing the SOMA type of this object."""
+        raise NotImplementedError()
 
 
 _ST = TypeVar("_ST", bound=SOMAObject)
@@ -25,3 +60,7 @@ class Collection(SOMAObject, MutableMapping[str, _ST]):
     """
 
     __slots__ = ()
+
+    @property
+    def soma_type(self) -> Literal["SOMACollection"]:
+        return "SOMACollection"
