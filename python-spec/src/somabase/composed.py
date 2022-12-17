28719ff6d61677f6a1a6a599e4d3519d0168df7a
Paul Fisher <paul.fisher@tiledb.com>
2022-12-16 22:18:56 -0500

    Implement `Measurement`, the first of the composed types.
    The SOMA composed types are implemented as concrete, instantiable
    classes that can be instantiated as wrappers around the
    implementation-specific Collection classes.
    
    When loading a composed SOMA object from storage, the "smart open"
    function of the implementation will identify the collection as a
    composed type, load it into its Collection implementation, and then
    wrap it with the appropriate behavior decorator.
    
        # In an open function:
        collection = load_collection_from_uri(uri)
        # After determining that the collection is an Experiment:
        exp = composed.Experiment(collection)
        # return `exp` to the user.

---
+++ python-spec/src/somabase/composed.py
@@ -0,0 +1,54 @@
+"""Implementations of the composed SOMA data types."""
+
+from typing_extensions import Final
+
+from somabase import _wrap
+from somabase import base
+from somabase import data
+
+
+class Measurement(_wrap.CollectionProxy):
+    """A set of annotated variables and values for a single measurement."""
+
+    __slots__ = ()
+
+    var = _wrap.item(data.DataFrame)
+    """Primary annotations on the variable axis for vars on this meansurement.
+
+    This annotates _columns_ of the ``X`` arrays. The contents of the
+    ``soma_joinid`` pseudo-column define the variable index domain (``varid``)
+    All variables for this measurement _must_ be defined in this dataframe.
+    """
+
+    X = _wrap.item(base.Collection[data.NDArray])
+    """A collection of matrices containing feature values.
+
+    Each matrix is indexed by ``[obsid, varid]``. Sparse and dense 2D arrays may
+    both be used in any combination in ``X``.
+    """
+
+    obsm = _wrap.item(base.Collection[data.DenseNDArray])
+    """Matrices containing annotations of each ``obs`` row.
+
+    This has the same shape as ``obs`` and is indexed with ``obsid``.
+    """
+
+    obsp = _wrap.item(base.Collection[data.SparseNDArray])
+    """Matrices containg pairwise annotations of each ``obs`` row.
+
+    This is indexed by ``[obsid_1, obsid_2]``.
+    """
+
+    varm = _wrap.item(base.Collection[data.DenseNDArray])
+    """Matrices containing annotations of each ``var`` row.
+
+    This has the same shape as ``var`` and is indexed with ``varid``.
+    """
+
+    varp = _wrap.item(base.Collection[data.SparseNDArray])
+    """Matrices containg pairwise annotations of each ``var`` row.
+
+    This is indexed by ``[varid_1, varid_2]``.
+    """
+
+    soma_type: Final = "SOMAMeasurement"
