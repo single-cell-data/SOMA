b868c12138e0c1f79b1b2eecd20d6efa7a285cab
Paul Fisher <paul.fisher@tiledb.com>
2022-12-16 22:00:00 -0500

    Add CollectionProxy, a wrapper to add behavior to collections.
    Some collection types have behaviors in addition to their standard
    "place to store other things" behavior, like the Experiment and
    Measurement type. These behaviors are identical across all
    implementations, so it is not necessary to have each implementation redo
    their own version.
    
    CollectionProxy allows somabase to implement simple proxies that
    decorate implementation collections with cross-implementation
    type-specific behaviors, while delegating the implementation-specific
    behaviors to the wrapped object.

---
+++ python-spec/testing/test_wrap.py
@@ -0,0 +1,112 @@
+import unittest
+
+from somabase import _wrap
+from somabase import base
+
+
+class CollectionStub(base.Collection):
+    def __init__(self, data):
+        self._data = data
+
+    def __delitem__(self, item):
+        del self._data[item]
+
+    def __getitem__(self, item):
+        return self._data[item]
+
+    def __iter__(self):
+        return iter(self._data)
+
+    def __len__(self):
+        return len(self._data)
+
+    def __setitem__(self, item, value):
+        self._data[item] = value
+
+    @property
+    def metadata(self):
+        raise RuntimeError("[metadata stub]")
+
+
+class TestWrapper(unittest.TestCase):
+    def test_sanity(self):
+        data = {}
+        stub = CollectionStub(data)
+        wrapped = _wrap.CollectionProxy(stub)
+
+        wrapped["x"] = "y"
+        self.assertIn("x", wrapped)
+        self.assertEqual(wrapped["x"], "y")
+        self.assertEqual(data["x"], "y")
+        self.assertEqual("y", wrapped.get("x", "z"))
+        self.assertIsNone(wrapped.get("missing"))
+        self.assertEqual("?", wrapped.get("missing", "?"))
+
+        del wrapped["x"]
+        self.assertNotIn("x", wrapped)
+        self.assertNotIn("x", data)
+
+        wrapped.update(a="b", c="d", e="f")
+
+        self.assertEqual("ace", "".join(wrapped))
+        self.assertEqual({"a", "c", "e"}, set(wrapped.keys()))
+        self.assertEqual("bdf", "".join(wrapped.values()))
+        self.assertEqual(
+            [("a", "b"), ("c", "d"), ("e", "f")],
+            list(wrapped.items()),
+        )
+        self.assertEqual(("a", "b"), wrapped.popitem())
+        self.assertEqual(2, len(wrapped))
+        self.assertEqual("f", wrapped.setdefault("e", "z"))
+        self.assertEqual("y", wrapped.setdefault("x", "y"))
+        with self.assertRaises(KeyError):
+            wrapped.pop("missing")
+        self.assertEqual("f", wrapped.pop("e"))
+        self.assertEqual(2, len(wrapped))
+        self.assertEqual("q", wrapped.pop("missing", "q"))
+        wrapped.clear()
+        self.assertEqual(len(wrapped), 0)
+
+        with self.assertRaisesRegex(RuntimeError, r"\[metadata stub\]"):
+            wrapped.metadata
+
+        self.assertIs(stub, wrapped.unwrap())
+        double_wrap = _wrap.CollectionProxy(wrapped)
+        self.assertIs(stub, double_wrap.unwrap())
+
+
+class TestItem(unittest.TestCase):
+    def test_get(self):
+        the_a = _wrap.item(str)
+
+        class ItemHaver(_wrap.CollectionProxy):
+
+            a = the_a
+            b = _wrap.item(int, "base_b")
+
+        self.assertIs(the_a, ItemHaver.a)
+
+        data = {}
+        items = ItemHaver(data)
+        items["c"] = "d"
+
+        with self.assertRaises(AttributeError):
+            items.a
+        with self.assertRaises(AttributeError):
+            items.b
+        with self.assertRaises(AttributeError):
+            del items.a
+
+        self.assertEqual("d", items["c"])
+
+        data["a"] = "a"
+        data["base_b"] = 1
+        self.assertEqual("a", items.a)
+        self.assertEqual(1, items.b)
+
+        items.a = "hello"
+        items.b = 500
+
+        self.assertEqual(dict(a="hello", base_b=500, c="d"), data)
+        del items.a
+        self.assertEqual(dict(base_b=500, c="d"), data)
