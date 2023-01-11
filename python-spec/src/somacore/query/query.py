12b033e86b9760070f1f14fc95917b3a401d6ea9
Paul Fisher <paul.fisher@tiledb.com>
2023-01-11 15:44:45 -0500

    Add `ExperimentAxisQuery`.
    This extracts the public interface of
    `tiledbsoma.query.ExperimentAxisQuery` and puts it here.
    
    Because there is no TileDB-specific behavior in `ExperimentAxisQuery`,
    I believe we can actually pull the entire implementation here (it only
    uses the properties of Experiment itself) but that is for a later date.

---
+++ python-spec/src/somacore/query/query.py
@@ -0,0 +1,112 @@
+import abc
+import contextlib
+from typing import Any, Optional, Sequence
+
+import anndata
+import pyarrow as pa
+from typing_extensions import TypedDict
+
+from somacore import data
+
+
+class AxisColumnNames(TypedDict, total=False):
+    """Specifies column names for experiment axis query read operations."""
+
+    obs: Optional[Sequence[str]]
+    """obs columns to use. All columns if ``None`` or not present."""
+    var: Optional[Sequence[str]]
+    """var columns to use. All columns if ``None`` or not present."""
+
+
+class ExperimentAxisQuery(contextlib.AbstractContextManager, metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def obs(
+        self, *, column_names: Optional[Sequence[str]] = None
+    ) -> data.ReadIter[pa.Table]:
+        """Returns ``obs`` as an Arrow table iterator."""
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def var(
+        self, *, column_names: Optional[Sequence[str]] = None
+    ) -> data.ReadIter[pa.Table]:
+        """Returns ``var`` as an Arrow table iterator."""
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def obs_joinids(self) -> pa.Array:
+        """Returns ``obs`` ``soma_joinids`` as an Arrow array."""
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def var_joinids(self) -> pa.Array:
+        """Returns ``var`` ``soma_joinids`` as an Arrow array."""
+        raise NotImplementedError()
+
+    @property
+    def n_obs(self) -> int:
+        """The number of ``obs`` axis query results."""
+        return len(self.obs_joinids())
+
+    @property
+    def n_vars(self) -> int:
+        """The number of ``var`` axis query results."""
+        return len(self.var_joinids())
+
+    @abc.abstractmethod
+    def X(self, layer_name: str) -> data.SparseRead:
+        """Returns an ``X`` layer as ``SparseRead`` data.
+
+        :param layer_name: The X layer name to return.
+        """
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def obsp(self, layer: str) -> data.SparseRead:
+        """Return an ``obsp`` layer as a SparseNDArrayRead"""
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def varp(self, layer: str) -> data.SparseRead:
+        """Return an ``varp`` layer as a SparseNDArrayRead"""
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def to_anndata(
+        self,
+        X_name: str,
+        *,
+        column_names: Optional[AxisColumnNames] = None,
+        X_layers: Sequence[str] = (),
+    ) -> anndata.AnnData:
+        """
+        Execute the query and return result as an ``AnnData`` in-memory object.
+
+        :param X_name: The name of the X layer to read and return
+            in the ``X`` slot.
+        :param column_names: The columns in the ``var`` and ``obs`` dataframes
+            to read.
+        :param X_layers: Additional X layers to read and return
+            in the ``layers`` slot.
+        """
+        raise NotImplementedError()
+
+    # Context management
+
+    @abc.abstractmethod
+    def close(self) -> None:
+        """Releases resources associated with this query.
+
+        This method must be idempotent.
+        """
+        raise NotImplementedError()
+
+    def __exit__(self, *_: Any) -> None:
+        self.close()
+
+    def __del__(self) -> None:
+        """Ensure that we're closed when our last ref disappears."""
+        # If any superclass in our MRO has a __del__, call it.
+        sdel = getattr(super(), "__del__", lambda: None)
+        sdel()
+        self.close()
