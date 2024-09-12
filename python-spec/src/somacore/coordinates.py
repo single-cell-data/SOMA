b02dd45e376fb52ef77e7bdc2ffc7579d10d502a
Julia Dark <julia@tiledb.com>
2024-09-12 17:41:12 -0400

    Add classes for coordinate spaces and transforms

---
+++ python-spec/src/somacore/coordinates.py
@@ -0,0 +1,392 @@
+"""Definitions of types related to coordinate systems."""
+
+import abc
+import collections.abc
+from dataclasses import dataclass
+from typing import Any, Optional, Sequence, Tuple, Union
+
+import numpy as np
+import numpy.typing as npt
+
+
+@dataclass
+class Axis(metaclass=abc.ABCMeta):
+    """A description of an axis of a coordinate system
+
+    Args:
+        name: Name of the axis.
+        unit: Optional string units. Defaults to ``None``.
+        scale: Optional scale for units. Defaults to ``None``.
+
+    Lifecycle: experimental
+    """
+
+    name: str
+    units: Optional[str] = None
+    scale: Optional[np.float64] = None
+
+
+class CoordinateSpace(collections.abc.Sequence):
+    """A coordinate system for spatial data."""
+
+    def __init__(self, axes: Sequence[Axis]):
+        self._axes = tuple(axes)
+        if len(self._axes) == 0:
+            raise ValueError("Coordinate space must have at least one axis.")
+        if len(set(axis.name for axis in self._axes)) != len(axes):
+            raise ValueError("The names for the axes must be unique.")
+
+    def __len__(self) -> int:
+        return len(self._axes)
+
+    def __getitem__(self, index: int) -> Axis:  # type: ignore[override]
+        return self._axes[index]
+
+    def __repr__(self) -> str:
+        output = f"<{type(self).__name__}\n"
+        for axis in self._axes:
+            output += f"  {axis},\n"
+        return output + ">"
+
+    @property
+    def axes(self) -> Tuple[Axis, ...]:
+        """TODO: Add docstring"""
+        return self._axes
+
+    @property
+    def axis_names(self) -> Tuple[str, ...]:
+        """TODO: Add docstring"""
+        return tuple(axis.name for axis in self._axes)
+
+    def rank(self) -> int:
+        """TODO: Add docstring"""
+        return len(self)
+
+
+class CoordinateTransform(metaclass=abc.ABCMeta):
+    """TODO: Add docstring"""
+
+    def __init__(
+        self,
+        input_axes: Union[str, Sequence[str]],
+        output_axes: Union[str, Sequence[str]],
+    ):
+        self._input_axes = (
+            (input_axes,) if isinstance(input_axes, str) else tuple(input_axes)
+        )
+        self._output_axes = (
+            (output_axes,) if isinstance(output_axes, str) else tuple(output_axes)
+        )
+
+    @abc.abstractmethod
+    def __matmul__(self, other: Any) -> "CoordinateTransform":
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def __rmatmul__(self, other: Any) -> "CoordinateTransform":
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def inverse_transform(self) -> "CoordinateTransform":
+        raise NotImplementedError()
+
+    @property
+    def input_axes(self) -> Tuple[str, ...]:
+        """TODO: Add docstring"""
+        return self._input_axes
+
+    @property
+    def input_rank(self) -> int:
+        """TODO: Add docstring"""
+        return len(self._input_axes)
+
+    @property
+    def output_axes(self) -> Tuple[str, ...]:
+        """TODO: Add docstring"""
+        return self._output_axes
+
+    @property
+    def output_rank(self) -> int:
+        """TODO: Add docstring"""
+        return len(self._output_axes)
+
+
+class AffineTransform(CoordinateTransform):
+    """TODO: Add docstring"""
+
+    def __init__(
+        self,
+        input_axes: Union[str, Sequence[str]],
+        output_axes: Union[str, Sequence[str]],
+        matrix: npt.ArrayLike,
+    ):
+        super().__init__(input_axes, output_axes)
+
+        # Check the rank of the input/output axes match.
+        if self.input_rank != self.output_rank:
+            raise ValueError(
+                "The input axes and output axes must be the same length for an "
+                "affine transform."
+            )
+        rank = self.input_rank
+
+        # Create and validate the augmented matrix.
+        self._matrix: npt.NDArray[np.float64] = np.array(matrix, dtype=np.float64)
+        if self._matrix.shape == (rank + 1, rank + 1):
+            if not (
+                self._matrix[-1, -1] == 1.0
+                and np.array_equal(self._matrix[-1, :-1], np.zeros((rank,)))
+            ):
+                raise ValueError(
+                    f"Input matrix {self._matrix} has augmented matrix shape, but is not a valid "
+                    f"augmented matrix."
+                )
+        elif self._matrix.shape == (rank, rank + 1):
+            self._matrix = np.vstack(
+                (
+                    self._matrix,
+                    np.hstack((np.zeros((rank,)), np.array([1]))),
+                )
+            )
+        else:
+            raise ValueError(
+                f"Unexpected shape {self._matrix.shape} for the input affine matrix."
+            )
+
+    def __matmul__(self, other: Any) -> CoordinateTransform:
+        if isinstance(other, CoordinateTransform):
+            if self.input_axes != other.output_axes:
+                raise ValueError("Axis mismatch between transformations.")
+            if isinstance(other, IdentityTransform):
+                return AffineTransform(other.input_axes, self.output_axes, self._matrix)
+            if isinstance(other, AffineTransform):
+                return AffineTransform(
+                    other.input_axes,
+                    self.output_axes,
+                    self.augmented_matrix @ other.augmented_matrix,
+                )
+        if isinstance(other, np.ndarray):
+            raise NotImplementedError(
+                "Support for multiplying by numpy arrays is not yet implemented."
+            )
+        raise TypeError(
+            f"Cannot multiply a CoordinateTransform by type {type(other)!r}."
+        )
+
+    def __rmatmul__(self, other: Any) -> CoordinateTransform:
+        if isinstance(other, CoordinateTransform):
+            if other.input_axes != self.output_axes:
+                raise ValueError("Axis mismatch between transformations.")
+            if isinstance(other, IdentityTransform):
+                return AffineTransform(
+                    self.input_axes, other.output_axes, self.augmented_matrix
+                )
+            if isinstance(other, AffineTransform):
+                return AffineTransform(
+                    self.input_axes,
+                    other.output_axes,
+                    other.augmented_matrix @ self.augmented_matrix,
+                )
+        if isinstance(other, np.ndarray):
+            raise NotImplementedError(
+                "Support for multiplying by numpy arrays is not yet implemented."
+            )
+        raise TypeError(
+            f"Cannot multiply a CoordinateTransform by type {type(other)!r}."
+        )
+
+    @property
+    def augmented_matrix(self) -> npt.NDArray[np.float64]:
+        """Returns the augmented affine matrix for the transformation."""
+        return self._matrix
+
+    def inverse_transform(self) -> CoordinateTransform:
+        """TODO: Add docstring"""
+        inv_a = np.linalg.inv(self._matrix[:-1, :-1])
+        b2 = -inv_a @ self._matrix[:-1, -1].reshape((self.output_rank, 1))
+        inv_augmented: npt.NDArray[np.float64] = np.vstack(
+            (
+                np.hstack((inv_a, b2)),
+                np.hstack((np.zeros(self.output_rank), np.array([1]))),
+            )
+        )
+        return AffineTransform(self.output_axes, self.input_axes, inv_augmented)
+
+
+class ScaleTransform(AffineTransform):
+    """TODO: Add docstring"""
+
+    def __init__(
+        self,
+        input_axes: Union[str, Sequence[str]],
+        output_axes: Union[str, Sequence[str]],
+        scale_factors: npt.ArrayLike,
+    ):
+        super(AffineTransform, self).__init__(input_axes, output_axes)
+        if self.input_rank != self.output_rank:
+            raise ValueError("Incompatible rank of input and output axes")
+
+        self._scale_factors: Union[np.float64, npt.NDArray[np.float64]] = np.array(
+            scale_factors, dtype=np.float64
+        )
+        if self._scale_factors.size == 1:
+            self._scale_factors = self._scale_factors.reshape((1,))[0]
+            self._isotropic = True
+        elif self._scale_factors.size == self.input_rank:
+            self._scale_factors = self._scale_factors.reshape((self.input_rank,))
+            self._isotropic = False
+        else:
+            raise ValueError(
+                f"Scale factors have unexpected shape={self._scale_factors.shape} "
+                f"for a transform with rank={self.input_rank}."
+            )
+
+    def __matmul__(self, other: Any) -> CoordinateTransform:
+        if isinstance(other, CoordinateTransform):
+            if self.input_axes != other.output_axes:
+                raise ValueError("Axis mismatch between transformations.")
+            if isinstance(other, ScaleTransform):  # Includes IdentityTransform
+                return ScaleTransform(
+                    other.input_axes,
+                    self.output_axes,
+                    self.scale_factors * other.scale_factors,
+                )
+            if isinstance(other, AffineTransform):
+                return AffineTransform(
+                    other.input_axes,
+                    self.output_axes,
+                    self.augmented_matrix @ other.augmented_matrix,
+                )
+        if isinstance(other, np.ndarray):
+            raise NotImplementedError(
+                "Support for multiplying by numpy arrays is not yet implemented."
+            )
+        raise TypeError(
+            f"Cannot multiply a CoordinateTransform by type {type(other)!r}."
+        )
+
+    def __rmatmul__(self, other: Any) -> CoordinateTransform:
+        if isinstance(other, CoordinateTransform):
+            if other.input_axes != self.output_axes:
+                raise ValueError("Axis mismatch between transformations.")
+            if isinstance(other, IdentityTransform):
+                return ScaleTransform(
+                    self.input_axes, other.output_axes, self._scale_factors
+                )
+            if isinstance(other, ScaleTransform):
+                return ScaleTransform(
+                    self.input_axes,
+                    other.output_axes,
+                    self._scale_factors * other._scale_factors,
+                )
+            if isinstance(other, AffineTransform):
+                return AffineTransform(
+                    self.input_axes,
+                    other.output_axes,
+                    other.augmented_matrix @ self.augmented_matrix,
+                )
+        if isinstance(other, np.ndarray):
+            raise NotImplementedError(
+                "Support for multiplying by numpy arrays is not yet implemented."
+            )
+        raise TypeError(
+            f"Cannot multiply a CoordinateTransform by type {type(other)!r}."
+        )
+
+    @property
+    def augmented_matrix(self) -> npt.NDArray[np.float64]:
+        """TODO: Add docstring"""
+        scales = np.append(self.scale_factors, [1.0])
+        return np.diag(scales)
+
+    def inverse_transform(self) -> CoordinateTransform:
+        """TODO: Add docstring"""
+        return ScaleTransform(
+            self.output_axes, self.input_axes, 1.0 / self._scale_factors
+        )
+
+    @property
+    def isotropic(self) -> bool:
+        """TODO: Add docstring"""
+        return self._isotropic
+
+    @property
+    def scale(self) -> np.float64:
+        """TODO: Add docstring"""
+        if not self._isotropic:
+            raise RuntimeError(
+                "Scale transform is not isotropic. Cannot get a single scale."
+            )
+        assert isinstance(self._scale_factors, np.float64)
+        return self._scale_factors
+
+    @property
+    def scale_factors(self) -> npt.NDArray[np.float64]:
+        """TODO: Add docstring"""
+        if self._isotropic:
+            assert isinstance(self._scale_factors, np.float64)
+            return np.array(self.input_rank * [self._scale_factors], dtype=np.float64)
+        assert isinstance(self._scale_factors, np.ndarray)
+        return self._scale_factors
+
+
+class IdentityTransform(ScaleTransform):
+    """TODO: Add docstring"""
+
+    def __init__(
+        self,
+        input_axes: Union[str, Sequence[str]],
+        output_axes: Union[str, Sequence[str]],
+    ):
+        super(AffineTransform, self).__init__(input_axes, output_axes)
+        if self.input_rank != self.output_rank:
+            raise ValueError("Incompatible rank of input and output axes")
+
+    def __matmul__(self, other: Any) -> CoordinateTransform:
+        if isinstance(other, CoordinateTransform):
+            if isinstance(other, IdentityTransform):
+                if other.output_axes != self.input_axes:
+                    raise ValueError("Axis mismatch between transformations.")
+                return IdentityTransform(other.input_axes, self.output_axes)
+            return other.__rmatmul__(self)
+        if isinstance(other, np.ndarray):
+            raise NotImplementedError(
+                "Support for multiplying by numpy arrays is not yet implemented."
+            )
+        raise TypeError(
+            f"Cannot multiply a CoordinateTransform by type {type(other)!r}."
+        )
+
+    def __rmatmul__(self, other: Any) -> CoordinateTransform:
+        if isinstance(other, CoordinateTransform):
+            if isinstance(other, IdentityTransform):
+                if other.output_axes != self.input_axes:
+                    raise ValueError("Axis mismatch between transformations.")
+                return IdentityTransform(self.input_axes, other.output_axes)
+            return other.__matmul__(self)
+        if isinstance(other, np.ndarray):
+            raise NotImplementedError(
+                "Support for multiplying by numpy arrays is not yet implemented."
+            )
+        raise TypeError(
+            f"Cannot multiply a CoordinateTransform by type {type(other)!r}."
+        )
+
+    @property
+    def augmented_matrix(self) -> npt.NDArray[np.float64]:
+        """Returns the augmented affine matrix for the transformation."""
+        return np.identity(self.input_rank + 1)
+
+    def inverse_transform(self) -> CoordinateTransform:
+        """TODO: Add docstring"""
+        return IdentityTransform(self.output_axes, self.input_axes)
+
+    @property
+    def isotropic(self) -> bool:
+        """TODO: Add docstring"""
+        return True
+
+    @property
+    def scale_factors(self) -> npt.NDArray[np.float64]:
+        """TODO: Add docstring"""
+        return np.array(self.input_rank * [1.0], dtype=np.float64)
---
+++ python-spec/testing/test_coordinates.py
@@ -0,0 +1,180 @@
+import numpy as np
+import pytest
+
+from somacore import AffineTransform
+from somacore import CoordinateTransform
+from somacore import IdentityTransform
+from somacore import ScaleTransform
+
+
+def check_transform_is_equal(
+    actual: CoordinateTransform, desired: CoordinateTransform
+) -> None:
+    assert actual.input_axes == desired.input_axes
+    assert actual.output_axes == desired.output_axes
+    if isinstance(desired, IdentityTransform):
+        assert isinstance(actual, IdentityTransform)
+    elif isinstance(desired, ScaleTransform):
+        assert isinstance(actual, ScaleTransform)
+        assert desired.isotropic == actual.isotropic
+        if desired.isotropic:
+            assert actual.scale_factors == desired.scale_factors
+        else:
+            np.testing.assert_array_equal(actual.scale_factors, desired.scale_factors)
+    elif isinstance(desired, AffineTransform):
+        assert isinstance(actual, AffineTransform)
+        np.testing.assert_array_equal(actual.augmented_matrix, desired.augmented_matrix)
+    else:
+        assert False
+
+
+@pytest.mark.parametrize(
+    ("input", "expected"),
+    [
+        (
+            AffineTransform(
+                ["x1", "y1"],
+                ["x2", "y2"],
+                [[2, 2, 0], [0, 3, 1]],
+            ),
+            np.array([[2, 2, 0], [0, 3, 1], [0, 0, 1]], np.float64),
+        )
+    ],
+)
+def test_affine_augmented_matrix(input, expected):
+    result = input.augmented_matrix
+    np.testing.assert_array_equal(result, expected)
+
+
+@pytest.mark.parametrize(
+    ("input", "expected"),
+    [
+        (
+            AffineTransform(
+                ["x1", "y1"],
+                ["x2", "y2"],
+                [[1, 0, 0], [0, 1, 0]],
+            ),
+            AffineTransform(
+                ["x2", "y2"],
+                ["x1", "y1"],
+                [[1, 0, 0], [0, 1, 0]],
+            ),
+        ),
+        (
+            AffineTransform(
+                ["x1", "y1"],
+                ["x2", "y2"],
+                [[1, 0, 5], [0, 1, 10]],
+            ),
+            AffineTransform(
+                ["x2", "y2"],
+                ["x1", "y1"],
+                [[1, 0, -5], [0, 1, -10]],
+            ),
+        ),
+        (
+            AffineTransform(
+                ["x1", "y1"],
+                ["x2", "y2"],
+                [[2, 0, -5], [0, 4, 5]],
+            ),
+            AffineTransform(
+                ["x2", "y2"],
+                ["x1", "y1"],
+                [[0.5, 0, 2.5], [0, 0.25, -1.25]],
+            ),
+        ),
+    ],
+)
+def test_inverse_transform(input, expected):
+    result = input.inverse_transform()
+    check_transform_is_equal(result, expected)
+    result_matrix = input.augmented_matrix @ result.augmented_matrix
+    expected_matrix: np.ndarray = np.identity(result.input_rank + 1, dtype=np.float64)
+    np.testing.assert_allclose(result_matrix, expected_matrix)
+
+
+@pytest.mark.parametrize(
+    ("transform_a", "transform_b", "expected"),
+    [
+        (
+            IdentityTransform(["x2", "y2"], ["x3", "y3"]),
+            IdentityTransform(["x1", "y1"], ["x2", "y2"]),
+            IdentityTransform(["x1", "y1"], ["x3", "y3"]),
+        ),
+        (
+            ScaleTransform(
+                ["x2", "y2"], ["x3", "y3"], np.array([1.5, 3.0], dtype=np.float64)
+            ),
+            IdentityTransform(["x1", "y1"], ["x2", "y2"]),
+            ScaleTransform(
+                ["x1", "y1"], ["x3", "y3"], np.array([1.5, 3.0], dtype=np.float64)
+            ),
+        ),
+        (
+            AffineTransform(
+                ["x2", "y2"],
+                ["x3", "y3"],
+                np.array([[1.5, 3.0, 0.0], [-1.5, 3.0, 1.0]], dtype=np.float64),
+            ),
+            IdentityTransform(["x1", "y1"], ["x2", "y2"]),
+            AffineTransform(
+                ["x1", "y1"],
+                ["x3", "y3"],
+                np.array([[1.5, 3.0, 0.0], [-1.5, 3.0, 1.0]], dtype=np.float64),
+            ),
+        ),
+        (
+            IdentityTransform(["x2", "y2"], ["x3", "y3"]),
+            ScaleTransform(
+                ["x1", "y1"], ["x2", "y2"], np.array([1.5, 3.0], dtype=np.float64)
+            ),
+            ScaleTransform(
+                ["x1", "y1"], ["x3", "y3"], np.array([1.5, 3.0], dtype=np.float64)
+            ),
+        ),
+        (
+            IdentityTransform(["x2", "y2"], ["x3", "y3"]),
+            AffineTransform(
+                ["x1", "y1"],
+                ["x2", "y2"],
+                np.array([[1.5, 3.0, 0.0], [-1.5, 3.0, 1.0]], dtype=np.float64),
+            ),
+            AffineTransform(
+                ["x1", "y1"],
+                ["x3", "y3"],
+                np.array([[1.5, 3.0, 0.0], [-1.5, 3.0, 1.0]], dtype=np.float64),
+            ),
+        ),
+        (
+            ScaleTransform(["x2", "y2"], ["x3", "y3"], [1.0, -1.0]),
+            ScaleTransform(["x1", "y1"], ["x2", "y2"], 1.5),
+            ScaleTransform(["x1", "y1"], ["x3", "y3"], [1.5, -1.5]),
+        ),
+        (
+            AffineTransform(
+                ["x2", "y2"],
+                ["x3", "y3"],
+                np.array([[2.0, 0.0, 1.0], [0.0, 4.0, 1.0]], dtype=np.float64),
+            ),
+            AffineTransform(
+                ["x1", "y1"],
+                ["x2", "y2"],
+                np.array([[1.0, 1.0, -1.0], [0.0, 1.0, 2.0]], dtype=np.float64),
+            ),
+            AffineTransform(
+                ["x1", "y1"],
+                ["x3", "y3"],
+                np.array([[2.0, 2.0, -1.0], [0.0, 4.0, 9.0]], dtype=np.float64),
+            ),
+        ),
+    ],
+)
+def test_multiply_tranform(
+    transform_a,
+    transform_b,
+    expected: CoordinateTransform,
+):
+    result = transform_a @ transform_b
+    check_transform_is_equal(result, expected)
