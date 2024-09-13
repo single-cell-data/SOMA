11118f6f9df2f8d87d9ec5243a57e94b934ed2c7
Julia Dark <julia@tiledb.com>
2024-09-13 15:04:16 -0400

    Add spatial presence matrices and use in ExperimentAxisQuery
    This adds dataframes for creating a join table between the scenes and
    observations/variables. New methods are added to the
    `ExperimentAxisQuery` class for getting a Arrow array of scene names
    that relate to the obs or var in the query.

--- python-spec/src/somacore/experiment.py
+++ python-spec/src/somacore/experiment.py
@@ -66,6 +66,17 @@ class Experiment(
     spatial = _mixin.item[_SceneColl]()  # TODO: Discuss the name of this element.
     """A collection of named spatial scenes."""
 
+    obs_spatial_presence = _mixin.item[_DF]()
+    """A dataframe that stores the presence of obs in the spatial scenes.
+
+    This provides a join table for the obs ``soma_joinid`` and the scene names used in
+    the ``spatial`` collection. This dataframe must contain dimensions ``soma_joinid``
+    and ``scene_id``. The ``scene_id`` dimension must be a ``string`` dimension. The
+    dataframe must contain a ``boolean`` column ``data``. The values of ``data`` are
+    ``True`` if the obs with obsid ``soma_joinid`` is contained in the scene
+    ``scene_id`` and ``False`` otherwise.
+    """
+
     def axis_query(
         self,
         measurement_name: str,
